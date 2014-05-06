575509b1dce78ac6ab3af0db7b81d0224099f988
VoteNixon <dementedsrt@gmail.com>
2014-05-06 19:08:08 -0400

    Add hit points for fatties. Hunger now reduces HPs when it is zero.

--- FME_main.py
+++ FME_main.py
@@ -1,17 +1,19 @@
-#Fatty Must Eat Version 0.0020
+#Fatty Must Eat Version 0.0021
 import time
 import random
 import array
 import csv
+import os
 
-log = open("FME_log.txt", "w") #open log file
-fme_conf = open('fatty_conf.csv', 'r') #open the configuration file
+log = open("c:/FME/FME_log.txt", "w") #open log file
+fme_conf = open('c:/FME/fatty_conf.csv', 'r') #open the configuration file
 
 fme_csv = csv.reader(fme_conf) #init csv reader
 #assign total fatties and max hunger from csv file
 for row in fme_csv:
     d = row[1]
     h = row[3]
+    hp = row[5]
 
 #User input for number of dudes
 #d = raw_input("Enter total number of fatties: ")
@@ -31,11 +33,11 @@ def foodSearch(h):
         food = 0
     return food
 
-
 c1 = 0 #hunger_skill counter
 c2 = 0 #dude simulator counter
 c3 = 0 #age output counter
 max_hunger = int(h)
+hp_max = int(hp)
 
 log.write("Total Fatties: " + d)
 log.write("\nMax Hunger: " + str(max_hunger) + "\n")
@@ -47,11 +49,13 @@ while c1 < array_max:
     log.write("\nFatty #" + str(c1 + 1) + " Hunger Skill: " +  str(dude_hunger_skill[c1]) + "\n")
     c1 += 1
 
+#Daily calculations for fatties
 while c2 < array_max:
     dead = 0
     day = 0
     hunger = max_hunger
     t0 = time.time()
+    fatty_hp = hp_max
     
     print("\nFatty #" + str(c2 + 1) + " calculating")
     log.write("\n")
@@ -60,16 +64,19 @@ while c2 < array_max:
     while dead == 0 and day < 100:
         t1 = time.time()
 
-        if (t1 - t0) >= 5.0:
+        if (t1 - t0) >= 2.0: #Each day is X.X seconds
             food_found = foodSearch(dude_hunger_skill[c2])
             hunger = hunger + food_found
             if hunger > max_hunger:
                 hunger = max_hunger
             log.write("\nDay " + str(day + 1) + " Hunger " + str(hunger))
-            log.write("\nYou found " + str(food_found) + " units of food\n") 
+            log.write("\nYou found " + str(food_found) + " units of food") 
             hunger -= 1
             day += 1
             if hunger <= 0:
+                fatty_hp -= 1
+            log.write("\nHP: " + str(fatty_hp) +"\n")
+            if fatty_hp <= 0:
                 dead = 1
     
     dude_age[c2] = day

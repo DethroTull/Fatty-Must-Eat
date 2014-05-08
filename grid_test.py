a0b2f32693438fbe0682818f889849626f5e2b3e
VoteNixon <dementedsrt@gmail.com>
2014-05-08 12:55:27 -0400

    grid_test now accepts movement points

--- grid_test.py
+++ grid_test.py
@@ -1,9 +1,10 @@
+#grid_test now takes movement points (mp)
 import random
 import array
 
 #base grid generation
-grid_x = 3 #total columns
-grid_y = 3 #total rows
+grid_x = 10 #total columns
+grid_y = 10 #total rows
 grid = [[0 for x in xrange(grid_x)] for x in xrange(grid_y)] #array to store the grid
 
 #populate the grid with random food values
@@ -19,11 +20,14 @@ while cx < grid_x:
 print(grid)
 print
 
+cm = 0 #mp loop counter
+mp = 2 #movement points per day
+
 #random starting location
 start_x = random.randrange(0,grid_x)
 start_y = random.randrange(0,grid_y)
 
-day_total = 11 #total time to run the simulation
+day_total = 101 #total time to run the simulation
 
 #simulation run loop
 cd = 0
@@ -35,25 +39,28 @@ while cd < day_total:
     print("Location: " + str(position_x) + "," + str(position_y))
     print("Food Found: " + str(grid[position_x][position_y]) + "\n")
 
-    move = random.randrange(0,4)
-    print("Move = " + str(move) + "\n")
+    while cm < mp:
+        move = random.randrange(0,4)
+        print("Move = " + str(move) + "\n")
 
-    if move == 0:
-        if position_x > 0 and position_x < grid_x:
-            position_x -= 1
-
-    if move == 1:
-        if position_y > -1 and position_y < (grid_y - 1):
-            position_y += 1
-
-    if move == 2:
-        if position_x > -1 and position_x < (grid_x - 1):
-            position_x += 1
-    
-    if move == 3:
-        if position_y > 0 and position_y < grid_y:
-            position_y -= 1
-
+        if move == 0:
+            if position_x > 0 and position_x < grid_x:
+                position_x -= 1
+                print("Up 1\n")
+        if move == 1:
+            if position_y > -1 and position_y < (grid_y - 1):
+                position_y += 1
+                print("Right 1\n")
+        if move == 2:
+            if position_x > -1 and position_x < (grid_x - 1):
+                position_x += 1
+                print("Down 1\n")
+        if move == 3:
+            if position_y > 0 and position_y < grid_y:
+                position_y -= 1
+                print("Left 1\n")
+        cm += 1
+    cm = 0
     cd += 1
 
 print("Simulation complete!")

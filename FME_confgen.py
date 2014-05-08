4af6e309bdbc47f0b5c922586ef4e9f7eb4a5d71
VoteNixon <dementedsrt@gmail.com>
2014-05-08 18:24:56 -0400

    Add map x and y variables

--- FME_confgen.py
+++ FME_confgen.py
@@ -8,10 +8,14 @@ fatty_conf = open("c:/FME/fatty_conf.csv", "w") #Open the csv file
 fatty_total = raw_input("Enter total number of fatties: ") 
 hunger_max = raw_input("Enter max hunger level: ")
 hp_max = raw_input("Enter max hitpoints: ")
+grid_x = raw_input("Enter map 'x' max: ")
+grid_y = raw_input("Enter map 'y' max: ")
 
 #Writing the variables to the csv file
 fatty_conf.write("fatty_total," + fatty_total)
 fatty_conf.write(",hunger_max," + hunger_max)
 fatty_conf.write(",hp_max," + hp_max)
+fatty_conf.write(",grid_x," + grid_x)
+fatty_conf.write(",grid_y," + grid_y)
 
 fatty_conf.close() #Close the csv file

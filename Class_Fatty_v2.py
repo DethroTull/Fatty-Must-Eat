88180bbfc81e92dee9095bba35b5ede108adf584
VoteNixon <dementedsrt@gmail.com>
2014-05-09 18:26:20 -0400

    no message

--- Class_Fatty_v2.py
+++ Class_Fatty_v2.py
@@ -4,13 +4,12 @@ class Fatty:
 
     def __init__ (self):
         print("A wild Fatty appears!")
+        self.int = random.randrange(0,3)
         self.max_hunger = 10
         self.hunger = 10
-        self.int = random.randrange(0,3)
         self.foraging = random.randrange(0,3)
-        self.hunting = random.randrange(0,3)
         self.max_hp = 5
         self.hp = 5
         self.max_ap = random.randrange(2,5)
         self.dead = 0
-
+        self.hunting = random.randrange(0,3)
